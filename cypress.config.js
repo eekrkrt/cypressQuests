a64bea0fd730828e4979a46842d6af026a4a1f00
eekrkrt <eekrkrt@gmail.com>
2024-07-05 15:55:24 +0200

    Add aggregated HTML report

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,30 @@ const { defineConfig } = require("cypress");
 const addDownloadPlugin = require('cypress-downloadfile/lib/addPlugin');
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
+  },
+  env: {
+    baseUrl: 'https://filebin.net/',
 
+  },
     setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
       on("task", { addDownloadPlugin });
+    
     },
-    env: {
-      baseUrl: 'https://filebin.net/',
-
+    reporterOptions: {
+      reportDir: 'cypress/reports/html',
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
+      reportFilename: 'aggregated-report.html',
+      reportTitle: 'Cypress Image Upload Test Report'
     }
-  }
+
+
+
 });
 
 
